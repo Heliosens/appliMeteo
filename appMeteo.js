abad926fffe0eab40958179eded10af6c6255c92
Sylvie-B <unsolo@hotmail.fr>
2021-12-17 12:04:54 +0100

    info traitement

--- appMeteo.js
+++ appMeteo.js
@@ -1,22 +1,31 @@
 // get place to write info
 let city = document.querySelector('header').querySelector('span');
+let time = document.getElementById('time');
 let description  = document.getElementById("description");
 
+// create xhr
 let xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Fourmies&lang=fr&appid=bb42fe4b0d1ec83e0f25ea6c292817ec");
 xhr.responseType = "json";
 
+// answer traitement
 xhr.onload = function () {
-    console.log(xhr.status);
+    // if fail quit function
     if(xhr.status !== 200){
-        console.log("fail " + xhr.status)
         return;
     }
 
+    // get response
     let info = xhr.response;
     console.log(info);
+    // write city name
     city.innerHTML = info.name;
+    // write weather description
     description.innerHTML = info.weather[0].description;
+
+    // write hour of sunrise
+    let time = new Date(info.sys.sunrise * 1000).toLocaleTimeString();
+
 }
 
 
